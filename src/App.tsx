7adba2e295dcbc711cf0f92a7ca3fce2dcd2413f
Imran Choudhary <cimran1307@gmail.com>
2026-06-28 17:08:50 +0530

    fixed errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import Navbar from './components/layout/Navbar';
 import Sidebar from './components/layout/Sidebar';
 import HeroSection from './components/sections/HeroSection';
